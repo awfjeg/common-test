7e6874fc66bac5e99ebceec76f4b99fa27aba3ae
awfjeg <awfjeg@163.com>
2021-07-09 15:09:21 +0800

    netty4 demo

---
+++ src/main/java/com/husher/netty4/DiscardServerHandler.java
@@ -0,0 +1,60 @@
+package com.husher.netty4;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * 处理服务端 channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // demo1: 默默地丢弃收到的数据
+//        ((ByteBuf) msg).release(); // (3)
+
+        // demo2: 查看收到的数据
+//        ByteBuf in = (ByteBuf) msg;
+//        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg); // (2)
+//        }
+
+        // demo3: 应答服务器
+        ctx.write(msg); // (1)
+        ctx.flush(); // (2)
+    }
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) { // (1)
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                ctx.close();
+            }
+        }); // (4)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
